948d005561059f0a72259795e3746a2ddfed817d
skn 2pac <skn2pac@gmail.com>
2024-09-12 17:46:59 -0300

    Se realiza un juego de ahorcado en JAVA en un curso

---
+++ src/Ahoracado.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Ahoracado {
+    public static void main(String[] args) throws Exception {
+
+        // Clase scanner para que el usuario escriba
+        Scanner scanner = new Scanner(System.in);
+
+        // Declaraciones y asignaciones
+        String palabraSecreta = "inteligencia";
+        int intentosMaximos = 10;
+        int intentos = 0;
+        boolean palabraAdivinada = false;
+
+        // Arreglos:
+        char[] letrasAdivinadas = new char[palabraSecreta.length()];
+
+        // Estructura de control: bucle
+        for (int i = 0; i < letrasAdivinadas.length; i++) {
+            letrasAdivinadas[i] = '_';
+        }
+
+        // Estructura de control: bucle
+        while (!palabraAdivinada && intentos < intentosMaximos) {
+            System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " ("
+                    + palabraSecreta.length() + " letras)");
+            System.out.println("Introduce una letra, por favor");
+
+            // Usamos la clase scanner para pedir una letra
+            char letra = Character.toLowerCase(scanner.next().charAt(0));
+
+            boolean letraCorrecta = false;
+            for (int i = 0; i < palabraSecreta.length(); i++) {
+                // Estructura de control condicional
+                if (palabraSecreta.charAt(i) == letra) {
+                    letrasAdivinadas[i] = letra;
+                    letraCorrecta = true;
+                }
+
+            }
+            if (!letraCorrecta) {
+                intentos++;
+                System.out.println("¡Incorrecto! Te quedan " + (intentosMaximos - intentos) + "intentos.");
+            }
+            if (String.valueOf(letrasAdivinadas).equals(palabraSecreta)) {
+                palabraAdivinada = true;
+                System.out.println("¡Felicidades has adivinado la palabra secreta: " + palabraSecreta);
+            }
+        }
+        if (!palabraAdivinada) {
+            System.out.println("¡Que pena te has quedado sin intentos! GAME OVER");
+        }
+        scanner.close();
+    }
+}
